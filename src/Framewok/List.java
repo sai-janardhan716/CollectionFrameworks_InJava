4a605bc77f0aa29de378d1fc04e58f2e5dbbdd73
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-11-20 10:11:46 +0530

    Added Pictorial Represenattion of List

--- src/Framewok/List.java
+++ src/Framewok/List.java
@@ -1,5 +1,15 @@
 package Framewok;
-
+// List is a first main Interface inherits the Collection interface
+/*
+    Collection
+    |__List (List interface extends Collection Interface)
+         |__ArrayList (ArrayList Class Implements List Interface)
+         |__Vector ( Vector Class Implements List Interface )
+         |     |
+         |     |__Stack (Stack class Extends Vector Class)
+         |__LinkedList ( LinkedList Class Implements List Interface )
+                       ( LinkedList Implements Deque Interface too )
+*/
 public interface List<E> extends Collection<E>{
     E get(int index);
     E set(int index, E element);
